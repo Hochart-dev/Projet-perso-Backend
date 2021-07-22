e90b154d6124b4d613c46a42e71c218ba769f4c5
Hochart-dev <hochart.dev@gmail.com>
2021-07-22 16:53:22 +0200

    add mail route and modificate conf.js

--- conf.js
+++ conf.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 const { DB_HOST, DB_USER, DB_SCHEMA, DB_PASSWORD } = process.env;
 
@@ -10,4 +11,17 @@ const connection = mysql.createPool({
   password: DB_PASSWORD,
 });
 
-module.exports = { db: connection };
+const transporter = nodemailer.createTransport({
+  service: process.env.MAIL_SERVICE,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+module.exports = {
+  db: connection,
+  mailer: transporter,
+};
